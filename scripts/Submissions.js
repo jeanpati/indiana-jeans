ea13fb8cdfbfcce07b2c5ad82f212d980ef69e85
Jean Patimeteeporn <jpatimet@gmail.com>
2024-12-05 19:16:43 -0600

    changed For...of loop rendering submissions to a .map

--- scripts/Submissions.js
+++ scripts/Submissions.js
@@ -1,17 +1,27 @@
 export const SubmissionList = async () => {
-    // Get the submissions from your API
-    const response = await fetch ("http://localhost:8088/submissions")
-    const submissions = await response.json()
+  // Get the submissions from your API
+  const response = await fetch("http://localhost:8088/submissions");
+  const submissions = await response.json();
 
-    // Iterate the submissions and create some <section> representations
-    let submissionHTML = ""
-    for (const submission of submissions) {
-        submissionHTML += `<section class="submission">
-            <div>Owns Jeans? ${submission.ownsBlueJeans}</div>
-            <div>Area type foreign key? ${submission.socioLocationId}</div>
-        </section>`
-    }
+  // Iterate the submissions and create some <section> representations
+  let submissionHTML = "";
+  // for (const submission of submissions) {
+  //     submissionHTML += `<section class="submission">
+  //         <div>Owns Jeans? ${submission.ownsBlueJeans}</div>
+  //         <div>Area type foreign key? ${submission.socioLocationId}</div>
+  //     </section>`
+  // }
 
-    // Return the HTML string
-    return submissionHTML
-}
+  // // Return the HTML string
+  // return submissionHTML
+
+  submissions.map((submission) => {
+    submissionHTML += `<section class="submission">
+        <div> Owns Jeans? ${submission.ownsBlueJeans}</div>
+        <div>Area Type Foreign Key: ${submission.socioLocationId}</div>
+    </section>`;
+  });
+
+  // Return the HTML string
+  return submissionHTML;
+};
